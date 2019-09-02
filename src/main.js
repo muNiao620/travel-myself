312dbe53f1f284a0320b12b76943efde7a1337a5
muNiao620 <a@adeMacBook-Pro.local>
2019-09-02 21:05:35 +0800

    change

--- src/main.js
+++ src/main.js
@@ -5,15 +5,22 @@ import App from './App'
 import router from './router'
 // 解决移动端有个300ms点击延迟的问题,在终端安装后fastclick后引入
 import fastClick from 'fastClick'
+// 导入轮播插件
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 // 设置每个页面的基础样式,styles是配置的路径别名
 import 'styles/reset.css'
 // 解决多倍屏里面1px边框变成多像素的问题
 import 'styles/border.css'
 //引入iconfont库
 import 'styles/iconfont.css'
+// 导入轮播css文件
+import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
+// 使用轮播插件
+Vue.use(VueAwesomeSwiper)
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="wrapper">
+    <swiper
+    :options="swiperOption">
+    <swiper-slide
+    v-for="item of swiperList"
+    :key="item.id">
+    	<img
+    	class="swiper-img"
+    	:src="item.imgUrl">
+
+    </swiper-slide>
+
+    <!-- 用来显示分页的 -->
+    <div class="swiper-pagination"  slot="pagination"></div>
+  	</swiper>
+  </div>
+</template>
+	
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+  	return {
+  		swiperOption: {
+  			pagination: ".swiper-pagination",
+  			// 轮播插件支持循环轮播
+  			loop: true
+  		},
+  		swiperList: [{
+  			id: "0001",
+  			imgUrl: "http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20198/083c708a3949958bcbca0fcc0ea3d57f.jpg_750x200_e47acfc7.jpg"
+  		}, {
+  			id: "0002",
+  			imgUrl: "http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20198/cad43eb25efc11ed763a0cded45a6bcf.jpg_750x200_3374a9eb.jpg"
+  		}]
+  	}
+  	
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+// >>>表示穿透，因为.swiper-pagination-bullet-active不是当前组件产生的类，不能为其他组件的类定义样式，所以通过穿透就可以定义.wrapper类下的任何出现.swiper-pagination-bullet-active的位置的样式
+	.wrapper >>> .swiper-pagination-bullet-active
+		background: #fff
+// .wrapper类的作用是解决，当页面刷新时，图片未加载出来的时候，出现的布局问题（图片不占流）
+	.wrapper
+		overflow: hidden
+		width: 100%
+		height: 0
+		padding-bottom: 26.4%
+		.swiper-img
+			width: 100%
+			
+			
+</style>
