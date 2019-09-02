863f3e0ca66047ce7440d648f75d55233d00fde5
muNiao620 <a@adeMacBook-Pro.local>
2019-09-02 16:39:48 +0800

    add header

--- src/main.js
+++ src/main.js
@@ -5,10 +5,12 @@ import App from './App'
 import router from './router'
 // 解决移动端有个300ms点击延迟的问题,在终端安装后fastclick后引入
 import fastClick from 'fastClick'
-// 设置每个页面的基础样式
-import './assets/styles/reset.css'
+// 设置每个页面的基础样式,styles是配置的路径别名
+import 'styles/reset.css'
 // 解决多倍屏里面1px边框变成多像素的问题
-import './assets/styles/border.css'
+import 'styles/border.css'
+//引入iconfont库
+import 'styles/iconfont.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,54 @@
+<template>
+	<div class="header">
+		<div class="header-left">
+			<!-- 引入icon图标，类名必须为iconfont，不然无法显示 -->
+			<div class="iconfont back-icon">&#xe624;</div>
+		</div>
+		<div class="header-input">
+			<span class="iconfont icon-back">&#xe632;</span>
+			输入城市/景点/游玩主题
+		</div>
+		<div class="header-right">
+			城市
+			<span class="iconfont arrow-icon">&#xe64a;</span>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+// 样式里面引入样式，需要加一个@符号
+// 第一种引用
+// @import '../../../assets/styles/varibles.styl'
+// 第二种引用~@，@表示src这个目录，是在配置中(webpack.base.conf)设置的，此处用@前面一定要加波浪线
+// 第三种引用 在webpack.base.conf，让styles表示src/assets/styles目录
+@import '~styles/varibles.styl' 
+	.header
+		display: flex
+		line-height: .88rem
+		background: $bgColor
+		color: #fff
+		.header-left
+			width: .4rem
+			padding: 0 .2rem
+			.back-icon
+				font-size: .36rem
+		.header-input
+			flex: 1
+			line-height: .6rem
+			margin: .14rem 0
+			padding-left: .2rem
+			background: #fff
+			border-radius: .05rem
+			color: #ccc
+		.header-right
+			padding: 0 .22rem
+			text-align: center
+			.arrow-icon
+				font-size: .24rem
+</style>
